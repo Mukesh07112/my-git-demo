f33751d19c744c47052709d4bf321d6d8a7e0862
Mukesh07112 <18euit085@skcet.ac.in>
2022-05-29 12:35:09 +0530

    first branch 'iss1' commit

--- src/com/cts/Main.java
+++ src/com/cts/Main.java
@@ -1,10 +1,16 @@
 package com.cts;
 
+import com.cts.service.Someservice;
+
 public class Main {
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome");
 		System.out.println("hello mukesh");
+		
+		Someservice ss=new Someservice();
+		System.out.println(ss.findLength("Program"));
+		
 		}
 
 }
---
+++ src/com/cts/service/Someservice.java
@@ -0,0 +1,7 @@
+package com.cts.service;
+
+public class Someservice {
+    public int findLength(String str) {
+    	return str.length();
+    }
+}
